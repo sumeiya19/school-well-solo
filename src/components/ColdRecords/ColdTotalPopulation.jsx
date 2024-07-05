50f04110c7919f5c73e63f809a2384b304a51b9f
Sumeiya Abdi <sumeiya619@gmail.com>
2024-07-05 17:19:36 -0500

    styles cold records page

--- src/components/ColdRecords/ColdTotalPopulation.jsx
+++ src/components/ColdRecords/ColdTotalPopulation.jsx
@@ -1,27 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { Box } from '@mui/material';
 
-function ColdTotalPopulation () {
+function ColdTotalPopulation() {
     const dispatch = useDispatch();
     const totalPopulation = useSelector((state) => state.totalPopulationReducer);
     const coldList = useSelector((state) => state.coldRecordsReducer);
 
     const totalIncidences = coldList.length;
 
-    const incidenceRate = totalPopulation > 0 ? (totalIncidences / totalPopulation) * 1000: 0;
+    const incidenceRate = totalPopulation > 0 ? (totalIncidences / totalPopulation) * 1000 : 0;
 
     useEffect(() => {
         dispatch({ type: "FETCH_TOTAL_POPULATION" });
-        // dispatch({ type: "FETCH_INCIDENCE" });
     }, [dispatch]);
-    return (<>
-        <div>
-            <p>Total Population: {totalPopulation}</p>
-            <p>Total Incidences: {totalIncidences}</p>
-            <p>Incidence Rate: {incidenceRate.toFixed(2)} per 1000</p>
-        </div>
-</>)
+
+    return (
+        <Box display="flex" justifyContent="center" alignItems="center" flexDirection="column" mt={4}>
+            <div>
+                <p style={{ fontFamily: 'Arial', fontSize: '18px', fontWeight: 'bold', textAlign: 'center' }}>Cold Incidence Statistics</p>
+                <p>Total Population: {totalPopulation}</p>
+                <p>Total Incidences: {totalIncidences}</p>
+                <p>Incidence Rate: {incidenceRate.toFixed(2)} per 1000</p>
+            </div>
+        </Box>
+    );
 }
 
-export default ColdTotalPopulation
+export default ColdTotalPopulation;
